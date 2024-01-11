1d25defb57bb991c4f2d2a37af01142a15a4775f
gaboLupus <i.s.c.guillermoespinoza@gmail.com>
2024-01-11 17:21:47 -0600

    SCRUM-4 2024_01_11__B_GGEC_SpEL_VALUES
    Obtencion de configuraciones con la anotacion Value

--- src/main/java/com/ggec/curso/springboot/web/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/ggec/curso/springboot/web/springbootweb/SpringbootWebApplication.java
@@ -3,9 +3,17 @@ package com.ggec.curso.springboot.web.springbootweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ */
 @SpringBootApplication
+
 public class SpringbootWebApplication {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWebApplication.class, args);
 	}
--- src/main/java/com/ggec/curso/springboot/web/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/ggec/curso/springboot/web/springbootweb/controllers/PathVariableController.java
@@ -1,8 +1,12 @@
 package com.ggec.curso.springboot.web.springbootweb.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +23,33 @@ import com.ggec.curso.springboot.web.springbootweb.models.dto.ParamDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String,Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private Integer price;
+
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}")
+    private List<String> valueList;
+
+    @Value("#{'${config.listOfValues}'.toUpperCase()}")
+    private String valueString;
+
+    @Autowired
+    private Environment environment;
 
     /**
      * 
@@ -58,6 +89,27 @@ public class PathVariableController {
         userModel.setName(userModel.getName().toUpperCase());
 
         return userModel;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("code2", environment.getProperty("config.code",Integer.class));
+        json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("listOfValues", listOfValues);
+        json.put("valueList", valueList);
+        json.put("valueString", valueString);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
+        return json;
     }
 
 }
