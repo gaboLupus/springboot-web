54da36fea5c446de23b15d69ae9345d0adac118b
gaboLupus <i.s.c.guillermoespinoza@gmail.com>
2024-01-11 18:21:36 -0600

    SCRUM-4 2024_01_11_1820_B_GGEC_REDIREC
    Redirecion de rutas con thymeleaf y redireccion de controllers

---
+++ src/main/java/com/ggec/curso/springboot/web/springbootweb/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.ggec.curso.springboot.web.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * 
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/","/home"})
+    public String home() {
+        return "redirect:/details";
+        // return "forward:/list";
+    }
+    
+}
